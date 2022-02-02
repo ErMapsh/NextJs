2c26e8fd04b7d77f7f44e6c15703e5d7a4222407
ErMapsh <maheshmestri73@gmail.com>
2022-02-02 22:10:30 +0530

    File Based Routing in Next.js

---
+++ Tutorials/tutorial2/pages/covidapp/Stat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Stat(){
+    return(
+        <div>This is stat in covid app</div>
+    )
+}
---
+++ Tutorials/tutorial2/pages/covidapp/map.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function map(){
+    return(
+        <div>This is map in covid app</div>
+    )
+}
