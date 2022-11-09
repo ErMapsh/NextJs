7874908fd1319e6ec696300acdeaae97a9924f11
ErMapsh <maheshmestri73@gmail.com>
2022-11-09 20:52:45 +0530

    dynamic rounting

---
+++ Tutorials/dynamic_routing3/README.md
@@ -0,0 +1,4 @@
+# Dynamic routing
+- dynamic routing is a thing, related to endpoint that hited in browser.
+- ie domain.com/blog/something a endpoint
+- blog having multiple post(things), so if we someone hit the wrong post that doesn't exist in database, that become helps us to identify
---
+++ Tutorials/dynamic_routing3/pages/blogs/[sno].js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+const Post = () => {
+    const router = useRouter()
+    const { sno } = router.query;
+    return (
+        <p style={{display: "flex", justifyContent: "center"}}>
+            Post: {sno}
+        </p>
+    )
+}
+
+export default Post;
